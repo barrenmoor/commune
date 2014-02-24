ba57511414a43c88ad7b560145f904dcac1c1380
barrenmoor <rajagopalk@gmail.com>
2014-02-24 21:41:26 +0530

    show sprint page and controller added

--- scripts/utils.js
+++ scripts/utils.js
@@ -10,6 +10,30 @@ var CommuneUtils = function() {
 			var day = days[d.getDay()];
 
 			return month + ' ' + date + ', ' + year + ', ' + day;
+		},
+
+		date_sort_asc : function (date1, date2) {
+			// This is a comparison function that will result in dates being sorted in
+			// ASCENDING order.
+			if (date1.getTime() < date2.getTime()) {
+				return -1;
+			} else if (date1.getTime() > date2.getTime()) {
+				return 1;
+			} else {
+				return 0;
+			}
+		},
+
+		convertToDate : function(arr) {
+			if(Object.prototype.toString.call(arr) === '[object Array]') {
+				var dates = [];
+				for(var i in arr) {
+					dates.push(new Date(arr[i]));
+				}
+				return dates;
+			} else {
+				return new Date(arr);
+			}
 		}
 	};
 }
