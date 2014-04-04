2747db2e5a5a8b73238654acc4a82958cfff1285
barrenmoor <rajagopalk@gmail.com>
2014-04-04 10:29:37 +0530

    changing context root

--- server.js
+++ server.js
@@ -11,18 +11,18 @@ app.use(function(req, res, next){
 //app.use(express.bodyParser()); //gives warning about deprecated api in connect module
 app.use(express.urlencoded());
 app.use(express.json());
-app.use('/commune', express.static(__dirname));
+app.use('/jive', express.static(__dirname));
 
-app.get('/commune/basic', links.basic);
+app.get('/jive/basic', links.basic);
 
-app.get('/commune/teams', links.teams);
-app.get('/commune/teams/:teamId', links.team);
+app.get('/jive/teams', links.teams);
+app.get('/jive/teams/:teamId', links.team);
 
-app.get('/commune/teams/:teamId/sprints', links.sprints);
-app.get('/commune/teams/:teamId/sprints/:sprintId', links.sprint);
-app.put('/commune/teams/:teamId/sprints/:sprintId', links.updateSprint);
-app.put('/commune/teams/:teamId/sprints/:sprintId/tasks', links.updateSprint);
-app.delete('/commune/teams/:teamId/sprints/:sprintId', links.deleteSprint);
-app.post('/commune/teams/:teamId/sprints', links.addSprint);
+app.get('/jive/teams/:teamId/sprints', links.sprints);
+app.get('/jive/teams/:teamId/sprints/:sprintId', links.sprint);
+app.put('/jive/teams/:teamId/sprints/:sprintId', links.updateSprint);
+app.put('/jive/teams/:teamId/sprints/:sprintId/tasks', links.updateSprint);
+app.delete('/jive/teams/:teamId/sprints/:sprintId', links.deleteSprint);
+app.post('/jive/teams/:teamId/sprints', links.addSprint);
 
 app.listen(8080);
